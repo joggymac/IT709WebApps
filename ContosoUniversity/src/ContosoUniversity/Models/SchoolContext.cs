d12b39e2aff23818ba9ceedc5255cda04186af9e
Ben Williams <Ben Williams@JIM>
2017-05-03 17:59:24 +1200

    The beginning of the end

---
+++ ContosoUniversity/src/ContosoUniversity/Models/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Models
+{
+    public class SchoolContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+
+    }
+}
