65a38e088ab37bedf7faeb43a7ec59c21dff3690
Ben Williams <chocolatepod54@aol.com>
2017-05-08 12:16:48 +1200

    tytry

---
+++ Databasing Tomi/Databasing Tomi/Script2.sql
@@ -0,0 +1,116 @@
+﻿/* SELECT LastName, FirstName from M_Customer
+inner JOIN M_Booking ON M_customer.CustID = M_Booking.CustID
+inner JOIN M_Movie On M_Booking.MovieID = M_Movie.MovieID
+WHERE M_Movie.Title = 'Walking Tall'
+order by LastName asc */
+
+
+/* SELECT Title FROM M_Movie
+inner JOIN M_Rental ON M_Movie.MovieID = M_Rental.MovieID
+inner JOIN M_Customer ON M_Rental.CustID = M_Customer.CustID
+WHERE M_Customer.LastName LIKE 'Black' AND M_Customer.FirstName LIKE 'Ray'
+ORDER BY M_Movie.Title ASC */
+
+/* SELECT StreetAddress FROM M_Supplier
+inner JOIN M_Movie ON M_Supplier.SupplierID = M_Movie.SupplierID
+WHERE M_Movie.Title = 'Emma' */
+
+/* SELECT LastName, FirstName, Phone FROM M_Customer
+INNER JOIN M_Rental ON M_Customer.CustID = M_Rental.CustID
+INNER JOIN M_Movie ON M_Rental.MovieID = M_Movie.MovieID
+WHERE M_Movie.Title = 'Apocalypse Now Redux'  */
+
+/* Create view BookingQuery AS
+SELECT LastName, Firstname, DateBooked, Title FROM M_customer, M_Booking, M_Movie */
+
+/*Create view ActionQuery AS
+SELECT LastName, Firstname, M_Booking.DateBooked, M_Movie.Title FROM M_customer, M_Booking, M_Movie
+WHERE M_Movie.Cat = 'ACT' */
+
+
+/*Create view RentalQuery AS
+SELECT LastName, FirstName, M_Movie.MovieID FROM M_Customer
+INNER JOIN M_Rental ON M_Customer.CustID = M_Rental.CustID
+INNER JOIN M_Movie ON M_Movie.MovieID = M_Rental.MovieID */
+
+/* SELECT LastName, FirstName, COUNT(M_Rental.MovieID) AS Rentals FROM M_Customer
+INNER JOIN M_Rental ON M_Customer.CustID = M_Rental.CustID
+INNER JOIN M_Movie ON M_Movie.MovieID = M_Rental.MovieID
+GROUP BY LastName, Firstname */
+
+/* SELECT DISTINCT LastName, FirstName FROM M_Customer
+INNER JOIN M_Rental ON M_Customer.CustID = M_Rental.CustID
+ORDER BY LastName ASC */
+
+/* SELECT Cat, Description FROM M_Category
+ORDER BY Description ASC */
+/*
+ SELECT DISTINCT CustID FROM M_Rental */
+
+/* SELECT LastName, FirstName, CustID FROM M_Customer
+ WHERE CustID = '8923' */
+
+/* SELECT CustID, MovieID FROM M_Booking
+WHERE MovieID = '7' */
+/*
+ SELECT MovieID FROM M_Booking
+WHERE CustID = '8922' */
+
+/* SELECT MovieID, Title FROM M_Movie
+WHERE PurchasePrice <= $50 */
+
+/* SELECT MovieID, Title FROM M_Movie
+WHERE PurchasePrice BETWEEN '70' AND '75' */
+/*
+ SELECT CustID, MovieID, CopyID, DueDate FROM M_Rental
+ORDER BY DueDate, MovieID */
+ /*
+SELECT Description FROM M_Category
+WHERE Description LIKE 'm%' */
+
+/* SELECT Cat, AVG(PurchasePrice) AS 'Average Purchase Price' FROM M_Movie
+GROUP BY Cat */
+/*
+ SELECT SupplierID, AVG(PurchasePrice) FROM M_Movie
+GROUP BY SupplierID */
+
+/* SELECT M_Customer.CustID, M_Customer.LastName, M_Customer.Firstname, COUNT (MovieID) AS 'Movies Booked' FROM M_Booking
+INNER JOIN M_Customer ON M_Booking.CustID = M_Customer.CustID
+GROUP BY M_Customer.CustID, M_Customer.LastName, M_Customer.Firstname */
+
+/* SELECT StreetAddress FROM M_Supplier
+INNER JOIN M_Movie ON M_Supplier.SupplierID = M_Movie.SupplierID
+WHERE M_Movie.Title = 'Kill Bill Vol. 1' */
+
+/*  SELECT LastName, FirstName, Phone FROM M_Customer
+
+INNER JOIN M_Rental ON M_Customer.CustID = M_Rental.CustID
+INNER JOIN M_Movie ON M_Rental.MovieID = M_Movie.MovieID
+WHERE M_Movie.Title = 'Kill Bill Vol. 2' */
+
+/* SELECT ClientOrderID, AVG(Quantity) AS Average FROM Order_Line
+GROUP BY ClientOrderID */
+/*
+ SELECT DISTINCT LastName, FirstName FROM M_Customer
+INNER JOIN M_Rental ON M_Customer.CustID = M_Rental.CustID
+ORDER BY LastName ASC*/ 
+
+/* SELECT Cat, AVG(PurchasePrice) AS AvgPurchPrice FROM M_Movie
+GROUP BY Cat */
+ 
+/* SELECT Cat, AVG(PurchasePrice) AS AvgPurchPrice FROM M_Movie
+GROUP BY Cat
+ORDER BY AvgPurchPrice */
+
+/* SELECT M_Supplier.SupplierID, AVG (M_Movie.PurchasePrice) AS SuppAvgPurchPrice FROM M_Supplier
+INNER JOIN M_Movie ON M_Supplier.SupplierID = M_Movie.SupplierID
+GROUP BY M_Supplier.SupplierID */
+
+/* SELECT CustID, COUNT(MovieID) AS MovieBook FROM M_Booking
+GROUP BY CustID */
+
+SELECT Cat, Description arge
+
+
+
+
---
+++ Databasing Tomi/Databasing Tomi/Script3.sql
@@ -0,0 +1,172 @@
+﻿-- Script to create and populate the tables for the Computer Sales system
+
+-- Drop the tables if they exist already
+
+DROP TABLE Order_Line;
+DROP TABLE Product;
+DROP TABLE ClientOrder;
+DROP TABLE Client;
+
+-- Create the database tables
+
+-- Create the Client table
+
+CREATE TABLE Client
+(ClientID      INT    PRIMARY KEY,
+ LastName      VARCHAR(20) NOT NULL,
+ FirstName     VARCHAR(20),
+ StreetAddress VARCHAR(40) NOT NULL,
+ Suburb        VARCHAR(20) NOT NULL,
+ City          VARCHAR(20) NOT NULL,
+ PhoneNumber   VARCHAR(10)
+);
+
+-- Create the ClientOrder table
+
+CREATE TABLE ClientOrder
+(ClientOrderID   INT  PRIMARY KEY,
+ OrderDate       DATE,
+ ClientID        INT  REFERENCES Client(ClientID),
+ Status          CHAR(1)    CHECK (Status IN ('U','N','P'))
+);
+
+-- Create the Product table
+
+CREATE TABLE Product
+(ProductID       INT    PRIMARY KEY,
+ Description     VARCHAR(30) NOT NULL,
+ InStock         INT,
+ Price           MONEY
+);
+
+--- Create the Order_Line table
+
+CREATE TABLE Order_Line
+(ClientOrderID  INT    REFERENCES ClientOrder(ClientOrderID),
+ ProductID      INT    REFERENCES Product(ProductID),
+                             PRIMARY KEY (ClientOrderID,ProductID),
+ Quantity       INT
+);
+
+BEGIN TRANSACTION ;
+
+-- Populate the tables with sample data
+
+-- Insert records into the Client table
+
+INSERT INTO Client
+ VALUES(1,	'Smith',	'Frank', '12 King Street',	'Avondale',	'Auckland',	'566-1234');
+INSERT INTO Client
+ VALUES(2,	'Jones',	'Sarah', '20 High Street',	'East',		'Christchurch',	'345-6565');
+INSERT INTO Client
+ VALUES(3,	'Connors',	'Fred',	 '34 Queen Street',	'Mt Roskill',	'Auckland',	'566-3333');
+INSERT INTO Client
+ VALUES(4,	'Headon',	'Jill',	 '103 King Street',	'Petoni',	'Wellington',	'333-2323');
+INSERT INTO Client
+ VALUES(5,	'Smith',	'Ben',	 '14 Princess Road',	'Kirkbridie',	'Wellington',	'454-8986');
+INSERT INTO Client
+ VALUES(6,	'Foster',	'Mike',	 '20 Viper Road',	'Mt Albert',	'Auckland',	'566-3433');
+INSERT INTO Client
+ VALUES(7,	'Edwards', 	'Steve', '31 Laleham',  	'Ponsonby',	'Wellington',	'444-3456');
+INSERT INTO Client
+ VALUES(8,	'Bentall', 	'Karen', '1 Jade Road', 	'South',	'Dunedin',	'332-2311');
+INSERT INTO Client
+ VALUES(9,	'Leech',	'Mick',	 '99 King Street',	'East',		'Christchurch',	'345-4343');
+INSERT INTO Client
+ VALUES(10,	'Gordon',	'Jo',	 '39 South Street',	'Avondale',	'Auckland',	'566-7777');
+
+
+-- Insert records into the ClientOrder table
+
+INSERT INTO ClientOrder
+ VALUES (101, 	'2010-02-20',	1,	'U');
+INSERT INTO ClientOrder
+ VALUES (102, 	'2010-02-21',	2,	'U');
+INSERT INTO ClientOrder
+ VALUES (103,	'2010-02-22',	3,	'U');
+INSERT INTO ClientOrder
+ VALUES (104,	'2010-02-23',	4,	'U');
+INSERT INTO ClientOrder
+ VALUES (105,	'2010-02-23',	6, 	'U');
+INSERT INTO ClientOrder
+ VALUES (106,	'2010-02-23',	8,	'U');
+INSERT INTO ClientOrder
+ VALUES (107,	'2010-02-24', 	1,	'U');
+INSERT INTO ClientOrder
+ VALUES (108,	'2010-02-24',	9,	'U');
+INSERT INTO ClientOrder
+ VALUES (109,	'2010-02-24',	2,	'U');
+INSERT INTO ClientOrder
+ VALUES (110,	'2010-02-25',	1,	'U');
+
+-- Insert records into the Product table
+
+INSERT INTO Product
+ VALUES (1,	'Sound Card',   	10,	75.50);
+INSERT INTO Product
+ VALUES (2,	'Network Card', 	12,	50.00);
+INSERT INTO Product
+ VALUES (3,	'RS232 Interface',	20,	20.00);
+INSERT INTO Product
+ VALUES (4,	'RJ45 Lead',    	34,	14.99);
+INSERT INTO Product
+ VALUES (5,	'I/O Card',     	50,	45.00);
+INSERT INTO Product
+ VALUES (6,	'10g Hard Drive',	25,	200.00);
+INSERT INTO Product
+ VALUES (7,	'15g Hard Drive',	24,	250.00);
+INSERT INTO Product
+ VALUES (8,	'Floppy Drive', 	2,	100.00);
+INSERT INTO Product
+ VALUES (9,	'Motherboard V1',	30,	275.00);
+INSERT INTO Product
+ VALUES (10,	'Motherboard V2',	40,	300.00);
+
+
+-- Insert records into the Order_Line table
+
+INSERT INTO Order_Line
+ VALUES (101,	1,	2);
+INSERT INTO Order_Line
+ VALUES (101,	8,	1);
+INSERT INTO Order_Line
+ VALUES (102,	2,	4);
+INSERT INTO Order_Line
+ VALUES (102,	3,	5);
+INSERT INTO Order_Line
+ VALUES (102,	6,	2);
+INSERT INTO Order_Line
+ VALUES (102,	7,	1);
+INSERT INTO Order_Line
+ VALUES (103,	4,	4);
+INSERT INTO Order_Line
+ VALUES (103,	5,	2);
+INSERT INTO Order_Line
+ VALUES (104,	8,	1);
+INSERT INTO Order_Line
+ VALUES (105,	3,	1);
+INSERT INTO Order_Line
+ VALUES (105,	4,	2);
+INSERT INTO Order_Line
+ VALUES (105,	5,	3);
+INSERT INTO Order_Line
+ VALUES (106,	1,	2);
+INSERT INTO Order_Line
+ VALUES (106,	2,	1);
+INSERT INTO Order_Line
+ VALUES (107,	2,	3);
+INSERT INTO Order_Line
+ VALUES (107,	3,	1);
+INSERT INTO Order_Line
+ VALUES (108,	2,	2);
+INSERT INTO Order_Line
+ VALUES (109,	3,	2);
+INSERT INTO Order_Line
+ VALUES (109,	8,	2);
+INSERT INTO Order_Line
+ VALUES (110,	5,	4);
+
+
+-- Permanently save all records inserted into the tables
+
+COMMIT TRANSACTION ;
