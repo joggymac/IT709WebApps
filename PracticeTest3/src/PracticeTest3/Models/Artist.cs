78b3a26068bb6592166bf1ed2e87355893cf8d5b
Ben Williams <Ben Williams@JIM>
2017-05-10 12:01:23 +1200

    PracticeTest

---
+++ PracticeTest3/src/PracticeTest3/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeTest3.Models
+{
+    public class Artist
+    {
+        public int ArtistID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
