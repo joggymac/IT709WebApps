b8e0e53523cf08f7d3fccb18f1523c6770c532f4
Ben Williams <chocolatepod54@aol.com>
2017-05-30 12:36:15 +1200

    dwd

---
+++ DatabaseAssignment2/DatabaseAssignment2/Attributes.sql
@@ -0,0 +1,14 @@
+﻿BEGIN TRANSACTION;
+
+INSERT INTO D_Product VALUES (001, 10, 'Puffy Jacket', 18, 'Jackets', 25);
+INSERT INTO D_Product VALUES (002, 16, 'Green T-Shirt', 16, 'Mens T-Shirt', 75);
+INSERT INTO D_Product VALUES (003, 10, 'Denom Jeans', 17, 'Unisex Pants', 50);
+INSERT INTO D_Product VALUES (004, 4, 'Brown Pants', 10, 'Boys Pants', 25);
+INSERT INTO D_Product VALUES (005, 25, 'Blue Pants', 10, 'Boys Pants', 25);
+
+INSERT INTO D_Order VALUES (1001, 001, 5, 'On Transit',  10);
+INSERT INTO D_Order VALUES (1002, 001, 2, 'Processing',  14);
+INSERT INTO D_Order VALUES (1003, 004, 2, 'Processing',  20);
+INSERT INTO D_Order VALUES (1003, 004, 2, '',  20);
+INSERT INTO D_Order VALUES (1003, 004, 2, 'Processing',  20);
+
--- DatabaseAssignment2/DatabaseAssignment2/Tables.sql
+++ DatabaseAssignment2/DatabaseAssignment2/Tables.sql
@@ -1,33 +1,38 @@
-﻿CREATE TABLE D_Product (
-ProductID			INT PRIMARY KEY,
+﻿DROP TABLE D_Sales_Rep
+DROP TABLE D_Customer
+DROP TABLE D_Order
+DROP TABLE D_Product
+
+CREATE TABLE D_Product (
+ProductID			INT NOT NULL PRIMARY KEY,
+No_of_Units			INT  NOT NULL,
 Description			VARCHAR(20) NOT NULL,
-No_of_Units			INT NOT NULL,
 Min_Level			INT NOT NULL,
-Cat					VARCHAR(20),
-Unit_Price			MONEY,
-Discount			INT
+Cat					VARCHAR(20) NOT NULL,
+Unit_Price			MONEY NOT NULL,
+Discount			INT NOT NULL
 )
 
 CREATE TABLE D_Order (
-OrderID				INT PRIMARY KEY,
-ProductID			INT PRIMARY KEY REFERENCES Product(ProductID),
-No_of_Units			INT REFERENCES Product(No_of_Units),
-Order_Date			DATE NOT NULL,
+OrderID				INT NOT NULL PRIMARY KEY,
+ProductID			INT NOT NULL REFERENCES D_Product(ProductID),
+No_of_Units			INT NOT NULL,
 Order_Status		VARCHAR(20) NOT NULL,
 Quoated_Price		MONEY NOT NULL
 )
 
 CREATE TABLE D_Customer (
 CustID				INT PRIMARY KEY,
-OrderID				INT PRIMARY KEY REFERENCES D_Order(OrderID),
+OrderID				INT REFERENCES D_Order(OrderID),
 Lname				VARCHAR(20) NOT NULL,
 Fname				VARCHAR(20) NOT NULL,
-Order_Date			DATE REFERENCES D_Order(Order_Date)
+Order_Date			DATE NOT NULL,
+
 )
 
 CREATE TABLE D_Sales_Rep (
-SalesrepID			INT PRIMARY KEY,
-CustID				INT PRIMARY KEY REFERENCES D_Customer(CustID),
+SalesrepID			INT NOT NULL PRIMARY KEY,
+CustID				INT NOT NULL REFERENCES D_Customer(CustID),
 Lname				VARCHAR(20) NOT NULL,
 Fname				VARCHAR(20) NOT NULL,
 )
