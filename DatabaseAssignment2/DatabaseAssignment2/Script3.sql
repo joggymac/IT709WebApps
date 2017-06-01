a80b0dee8890c6e001ba973adbf3b2600db766f7
2015009543 <2015009543@INV-VDI-BIT065>
2017-06-01 14:30:29 +1200

    htfhfhtf

--- DatabaseAssignment2/DatabaseAssignment2/Script3.sql
+++ DatabaseAssignment2/DatabaseAssignment2/Script3.sql
@@ -1,4 +1,11 @@
 ﻿CREATE PROCEDURE ProductDiscountRaise
- (
- @Discount
- )
+(
+	@Discount int
+)
+	AS
+	BEGIN
+	IF (@Discount < 1) OR (@Discount > 1)
+	UPDATE D_Product SET @Discount = Discount + 1;
+	ELSE 
+	PRINT 'Error: Value must be greater than 0 and less than 1'
+END
