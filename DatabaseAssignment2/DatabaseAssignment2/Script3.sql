634ddc42233cf0ba2ee9cf7aed836756bdeae247
Ben Williams <Ben Williams@JIM>
2017-06-06 18:03:40 +1200

    huhui

--- DatabaseAssignment2/DatabaseAssignment2/Script3.sql
+++ DatabaseAssignment2/DatabaseAssignment2/Script3.sql
@@ -4,10 +4,14 @@
 )
 	AS
 	BEGIN
-	IF EXISTS (Select * FROM D_Product WHERE @Discount > 0 OR @Discount < 1)
+	IF EXISTS (Select ProductID FROM D_Product WHERE Discount > 0 OR Discount < 1)
 	Update D_Product SET Discount = @Discount + 1;
 	ELSE 
 	PRINT 'Error: Value must be greater than 0 and less than 1'
 END
 
+DROP PROCEDURE ProductDiscountRaise;
+
 EXEC ProductDiscountRaise 0
+
+SELECT Discount FROM D_Product
--- DatabaseAssignment2/DatabaseAssignment2/Trigger2.sql
+++ DatabaseAssignment2/DatabaseAssignment2/Trigger2.sql
@@ -1,6 +1,17 @@
-﻿CREATE Trigger DeletedProduct
-on db.D_Product
+﻿CREATE TRIGGER  deletedprouduct ON dbo.D_Product
 INSTEAD OF DELETE
 AS
-BEGIN 
-DELETE FROM D_Order WHERE 
+	DECLARE @ProductID int;
+	DECLARE @OrderID int;
+
+
+	SELECT @ProductID=d.ProductID FROM DELETED d;
+	SELECT @OrderID=d.OrderID FROM DELETED d;
+
+	BEGIN 
+			DELETE FROM D_Customer_Orders WHERE OrderID = @OrderID
+			DELETE FROM D_Order WHERE OrderID = @OrderID;
+			DELETE FROM D_Product WHERE ProductID = @ProductID
+	END
+
+
